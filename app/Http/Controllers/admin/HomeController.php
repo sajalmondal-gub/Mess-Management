4a40466d34a1ea1b5b0820ee54ce57252243ffbf
sajalmondal-gub <sajalmondal.gub@gmail.com>
2023-12-24 19:50:54 +0600

    admin dashboard initialize

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers\admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
     public function index(){
-        $admin =Auth::guard('admin')->user();
-        echo 'is loged in'.$admin->name. '<a href="'.route('admin.logout').'">logout</a>';
+        return View('admin.dashboard');
     }
     public function logout(){
         Auth::guard('admin')->logout();
--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix'=>'admin'],function(){
-    Route::group(['middleware'=>'admin.guest'],function(){
-        Route::get('/login',[AdminController::class,'index'])->name('admin.login');
-        Route::post('/authenticate',[AdminController::class,'authenticate'])->name('admin.authenticate');
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middleware' => 'admin.guest'], function () {
+        Route::get('/login', [AdminController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminController::class, 'authenticate'])->name('admin.authenticate');
 
     });
-    Route::group(['middleware'=>'admin.auth'],function(){
+    Route::group(['middleware' => 'admin.auth'], function () {
 
-        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
-        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
     });
-   
+
 });
