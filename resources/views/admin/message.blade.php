c86fa79b84ff1c66ac8246b60eb406423bc8302a
sajalmondal-gub <sajalmondal.gub@gmail.com>
2023-12-26 21:16:31 +0600

    update message and users

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -1,11 +1,11 @@
 @if(Session::has('error'))
-
-<div class="alert alert-danger alert-dismissible p-2">   
-     {{ Session::get('error') }}
-</div>
+    <div class="alert alert-danger alert-dismissible p-2">
+        {{ Session::get('error') }}
+    </div>
 @endif
+
 @if(Session::has('success'))
-<div class="alert alert-success alert-dismissible" p-2>
-   {{ Session::get('success') }}
-</div>
-@enderror
+    <div class="alert alert-success alert-dismissible p-2">
+        {{ Session::get('success') }}
+    </div>
+@endif
--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -1,57 +1,93 @@
 @extends('admin.layouts.app')
 @section('content')
-@include('admin.message')
-<div class="card border-0">
-
-    <div class="card-body table-responsive p-0">
-        <table class="table  table-hover text-nowrap">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th width="100">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($users->isNotEmpty())
-                @foreach($users as $user)
-                <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->phone }}</td>
-                    <td>
-                   <a href=""  onclick="deleteCategory({{ $user->id }})"><i class="fa fa-trash" aria-hidden="true"></i></a>
-                    </td>
-                </tr>
-                @endforeach
-                @endif
-            </tbody>
-        </table>
+<section class="content-header">
+    <div class="container-fluid my-2">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Mess Member</h1>
+            </div>
+        </div>
     </div>
-</div>
+    <!-- /.container-fluid -->
+</section>
+<!-- Main content -->
+<section class="content">
+    <!-- Default box -->
+    <div class="container-fluid">
+        @include('admin.message')
+        <div class="card">
+
+            <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                    <thead>
+                        <tr>
+                            <th width="60">ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>                          
+                            <th width="100">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if($users->isNotEmpty())
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->phone }}</td>
+                            <td>
+                                <a href="" onclick="deleteuser({{ $user->id }})" class="text-danger w-4 h-4 mr-1">
+                                    <svg wire:loading.remove.delay="" wire:target=""
+                                        class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path ath fill-rule="evenodd"
+                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @else
+                        <tr>
+                            <td colspan="5">Records Not Found</td>
+                        </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer clearfix">               
+            </div>
+        </div>
+    </div>
+    <!-- /.card -->
+</section>
+<!-- /.content -->
+
 @endsection
 @section('customJs')
 <script>
-	function deleteCategory(id){
-		var url='{{ route("users.delete","ID") }}';
-		var newUrl = url.replace("ID",id)
-		if (confirm("Are you sure. you want to delete")) {
-			$.ajax({
-				url: newUrl,
-				type: 'delete',
-				data: {},
-				dataType: 'json',
-				headers: {
-					      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-				         },
-				success: function(response){
-					if (response["status"]==true) {
-						window.location.href="{{ route('users.index') }}";
-					}
-				}	
-		   });	
-		}	
-	}
+    function deleteuser(id) {
+        var url = '{{ route("users.delete","ID") }}';
+        var newUrl = url.replace("ID", id)
+
+        if (confirm("Are you sure. you want to delete")) {
+            $.ajax({
+                url: newUrl,
+                type: 'delete',
+                data: {},
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                success: function (response) {
+                    if (response["status"]==true) {
+                        window.location.href = "{{ route('users.index') }}";
+                    }
+                }
+            });
+        }
+    }
 </script>
 @endsection
